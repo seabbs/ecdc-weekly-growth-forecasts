6abab4daf1a232cfc961c9a3d4a4e025a94be555
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-03-29 10:44:21 +0100

    remove double inc

--- R/format_forecasts.R
+++ R/format_forecasts.R
@@ -1,6 +1,6 @@
 format_forecasts <- function(forecasts, forecast_date, type = "cases", point = FALSE, min_horizon = -3, max_horizon = 4) {
   type <- match.arg(type, c("cases", "hospitalizations"))
-  hub_label <- c(cases = "inc case", hospitalizations = "inc hosp")
+  hub_label <- c(cases = "case", hospitalizations = "hosp")
 
   forecasts <- forecast.vocs::fv_extract_forecast(
     posterior
